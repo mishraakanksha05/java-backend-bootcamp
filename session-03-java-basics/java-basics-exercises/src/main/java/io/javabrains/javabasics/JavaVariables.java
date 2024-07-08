5d4e47cc356604edb6b3409763f122fd2da86689
mishraakanksha05 <159925688+mishraakanksha05@users.noreply.github.com>
2024-07-08 13:07:23 +0530

    Update JavaVariables.java

--- session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/JavaVariables.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/JavaVariables.java
@@ -5,27 +5,36 @@ public class JavaVariables {
     public static void main(String[] args) {
         // Step 1: Declare and initialize variables of different data types
         // int variable to store an integer value
+        int i = 2;
 
         // long variable to store a long value
+        long l = 256L;
 
         // float variable to store a floating-point value
+        float k = 2.314f;
 
 
         // double variable to store a double value
-
-
+        double d = 2.314;
+        
         // char variable to store a single character
+        char c = 'A';
 
 
         // boolean variable to store a boolean value
-
+        boolean b = true;
 
         // String variable to store a string of characters
-
+        String s = "Anurag";
 
         // Step 2: Print the values of the variables to the console
-
-
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(k);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(s);
         // Step 3: Modify the values of the variables and print the updated values
 
     }
